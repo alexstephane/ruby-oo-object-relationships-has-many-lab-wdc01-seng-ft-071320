8b668d6ee6f600a625f919d9d5b2766e8caa78d5
Alex Ngundji <alexngundji@gmail.com>
2020-07-18 18:59:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,16 +3,16 @@ class Artist
   attr_accessor :name
    #@@song_count = 0
   
-  
+  def songs
+    @songs
+  end
   def initialize(name)
    @name=name
    @songs =[]
     
   end
   
-   def songs
-    @songs
-  end
+   
   
   
 end
